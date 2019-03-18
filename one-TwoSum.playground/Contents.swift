c4ab3a5db77a800f9c0e2353c1129667869d1fb9
kailong <>
2019-03-18 15:16:16 +0800

    Bit侠
    twoSum

---
+++ one-TwoSum.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+class Solution {
+    var nums: [Int];
+    var target: Int;
+    
+    
+    init (_ nums:[Int], _ target:[Int]); -> [Int]{
+        var targets = [Int]();
+        for num in nums {
+            print(num);
+            
+        }
+        return targets;
+    };
+    
+}
+
+[2, 7, 11, 15]
+
+
